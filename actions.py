ea4a9408ffed4b8805f20da5d8d1e9864aa9dfef
Caleb Eubanks <ceubanks@headstorm.email>
2020-04-12 21:07:50 -0500

    got conversation stories and  training data to a nice stable state

--- actions.py
+++ actions.py
@@ -5,7 +5,7 @@
 # https://rasa.com/docs/rasa/core/actions/#custom-actions/
 
 
-from typing import Any, Dict, List, Text, Union
+from typing import Any, Dict, List, Text
 from rasa_sdk import Tracker, Action
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import UserUtteranceReverted
@@ -14,16 +14,41 @@ from rasa_sdk.forms import FormAction
 
 
 class CovidForm(FormAction):
-    """Collects information then queries from api"""
+    '''Collects information then queries from api'''
 
     def name(self):
-        return "covid_form"
+        return 'covid_form'
 
     @staticmethod
     def required_slots(tracker):
         return [
-          'location',
+          'country',
         ]
+
+    def slot_mappings(self) -> Dict[Text, Any]:
+        return {
+            'country': self.from_entity(
+                entity='country',
+                intent=[
+                    'inform',
+                    'ask_covid',
+                ],
+            ),
+            'case_type': self.from_entity(
+                entity='case_type',
+                intent=[
+                    'inform',
+                    'ask_covid',
+                ],
+            ),
+            'stat_type': self.from_entity(
+                entity='stat_type',
+                intent=[
+                    'inform',
+                    'ask_covid',
+                ]
+            )
+        }
 
     def submit(
         self,
@@ -31,17 +56,41 @@ class CovidForm(FormAction):
         tracker: Tracker,
         domain: Dict[Text, Any],
     ) -> List[Dict]:
-        dispatcher.utter_message("TO BE IMPLEMENTED")
+        country = tracker.get_slot('country')
+        dispatcher.utter_message(
+            f'Looks like you\'re trying to ask about covid-19 in {country}'
+        )
         return []
 
 
-class ActionGreetUser(Action):
-    """Revertible mapped action for utter_greet"""
+class ActionGreet(Action):
+    '''Revertible mapped action for utter_greet'''
 
     def name(self):
-        return "action_greet"
+        return 'action_greet'
 
     def run(self, dispatcher, tracker, domain):
-        dispatcher.utter_template("utter_greet", tracker)
+        dispatcher.utter_message(template='utter_greet')
         return [UserUtteranceReverted()]
 
+
+class ActionBye(Action):
+    '''Revertible mapped action for utter_bye'''
+
+    def name(self):
+        return 'action_bye'
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(template='utter_bye')
+        return [UserUtteranceReverted()]
+
+
+class ActionThank(Action):
+    '''Revertible mapped action for utter_noworries'''
+
+    def name(self):
+        return 'action_thank'
+
+    def run(self, dispatcher, tracker, domain):
+        dispatcher.utter_message(template='utter_noworries')
+        return [UserUtteranceReverted()]
--- data/stories.md
+++ data/stories.md
@@ -1,36 +1,23 @@
-## thank
-* thank
-  - utter_noworries
-
-## goodbye
-* bye
-  - utter_bye
-
 ## Some question from FAQ
 * faq
     - respond_faq
+    - action_restart
 
 ## covid form
 * ask_covid
     - covid_form
     - form{"name": "covid_form"}
     - form{"name": null}
+    - action_restart
 
-## covid form
-* ask_covid
+## covid form with country included
+* ask_covid{"country":"Ireland"}
     - covid_form
     - form{"name": "covid_form"}
     - form{"name": null}
-
-## escape form
-* ask_covid
-    - covid_form
-    - form{"name": "covid_form"}
-* faq
-    - respond_faq
-    - covid_form
-    - form{"name": null}
+    - action_restart
 
 ## out of scope
 * out_of_scope
     - utter_out_of_scope
+    - action_restart
